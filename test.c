31a9a4d68282b2fcc922418784c18fdb4ea7e8d3
Ahnaf101109 <148791555+ahnaf12c@users.noreply.github.com>
2026-06-22 12:41:02 +0600

    Delete test.c

--- test.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include"projectingAndRotating.h"
-
-int main () {
-    double r = DegreeToRadian(45);
-    Coords3d R = Rotate3DInX(20, 0, 20, r);
-    printf("%lf  %lf  %lf\n", R.x, R.y, R.z);
-    return 0;
-}
